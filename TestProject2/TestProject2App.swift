6a1379a4d019ab610058298b76879ab07441899a
AGodbole3 <145606796+AGodbole3@users.noreply.github.com>
2024-07-12 13:37:45 -0700

    Initial Commit

---
+++ TestProject2/TestProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestProject2App.swift
+//  TestProject2
+//
+//  Created by Anushree Godbole on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TestProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
